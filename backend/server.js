4374902f3ffc0a801a328e9a24b845961dbace80
Swaraj Mohaptra <110276137+swarajcodes@users.noreply.github.com>
2025-07-31 19:11:30 +0530

    prepper backend

--- backend/server.js
+++ backend/server.js
@@ -2,26 +2,53 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+const sessionRoutes = require("./routes/sessionRoutes");
+const questionRoutes = require("./routes/questionRoutes");
+const errorHandler = require("./middlewares/errorHandler");
+const { protect } = require("./middlewares/authMiddleware");
+const {
+  generateInterviewQuestions,
+  generateConceptExplanationQuestions,
+} = require("./controllers/aiController");
 
 const app = express();
 
-// Middleware to handle CORS
+//Middleware  to handle cors
 app.use(
   cors({
     origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    allowedHeaders: ["Content-type", "Authorization"],
   })
 );
 
-// Middleware
+connectDB();
+
+//Middlewares
 app.use(express.json());
 
-// Routes
+//Routes
+app.use("/api/auth", authRoutes);
+app.use("/api/sessions", sessionRoutes);
+app.use("/api/questions", questionRoutes);
 
-// Serve uploads folder
+app.use("/api/ai/generate-questions", protect, generateInterviewQuestions);
+app.use(
+  "/api/ai/generate-explanation",
+  protect,
+  generateConceptExplanationQuestions
+);
+
+// Error handler should be the last middleware
+app.use(errorHandler);
+
+//Serve uploads folder
 app.use("/uploads", express.static(path.join(__dirname, "uploads"), {}));
 
-//start Server
+//start server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
